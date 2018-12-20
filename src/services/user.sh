ccd9d0292ca4288137ae3c27a15f8bd5d3cbaceb
Robin Liu <commits@codemy.fun>
2018-12-21 07:19:33 +1100

    Fix authorized keys not being added correctly

--- src/services/user.sh
+++ src/services/user.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-jq -r '.config.cluster.identity.ssh.ubuntu.authorized_keys | @tsv' "$DEPLOY_SPEC" |
-  while IFS=$'\t' read -r key; do
-    echo "$key" >> "/home/ubuntu/authorized_keys"
-    chown ubuntu:ubuntu "/home/ubuntu/authorized_keys"
-    chmod 0600 "/home/ubuntu/authorized_keys"
+jq -r '.config.cluster.identity.ssh.ubuntu.authorized_keys | to_entries[] | .value' "$DEPLOY_SPEC" |
+  while IFS=$'\n' read -r key; do
+    echo "$key" >> "/home/ubuntu/.ssh/authorized_keys"
+    chown ubuntu:ubuntu "/home/ubuntu/.ssh/authorized_keys"
+    chmod 0600 "/home/ubuntu/.ssh/authorized_keys"
   done
